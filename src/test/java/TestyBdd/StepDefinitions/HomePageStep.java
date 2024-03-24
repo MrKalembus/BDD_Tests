22d3fdbf281125ab35cb5a7623f4847ed79c023c
MrKalembus <156577193+MrKalembus@users.noreply.github.com>
2024-03-24 11:45:04 +0000

    2

--- src/test/java/TestyBdd/StepDefinitions/HomePageStep.java
+++ src/test/java/TestyBdd/StepDefinitions/HomePageStep.java
@@ -3,6 +3,8 @@ package TestyBdd.StepDefinitions;
 import TestyBdd.Pages.HomePage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
 
 public class HomePageStep {
 
@@ -15,7 +17,17 @@ public class HomePageStep {
 
     @Then("I am on the main site")
     public void iAmOnTheMainSite() {
+        String nazwa = homePage.getHomepageName();
+        Assertions.assertEquals("Generic Shop", nazwa);
 
+    }
+
+    @When("Fill newsletter name with {string}")
+    public void fillNewsletterNameWith(String arg0) {
+    }
+    @When("Fill newsletter email with {string}")
+    public void fillNewsletterEmailWith(String arg0) {
+        
     }
 
 
--- src/test/resources/HomePageTests.feature
+++ src/test/resources/HomePageTests.feature
@@ -1,5 +1,13 @@
 Feature: Tests of home page
 
-  Scenario: Test strony głównej
+  Background:
     Given Browser is open
+
+  Scenario: Test strony głównej
+
     Then I am on the main site
+
+  Scenario: Zapisanie do Newsletter
+
+    When Fill newsletter name with "login"
+    And Fill newsletter email with "test@test.test"
